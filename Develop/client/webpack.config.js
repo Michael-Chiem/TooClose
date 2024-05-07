e606ba0da144b05f90588e80c7f758aeb1dd23f0
Michael Chiem <michaelchiem1@hotmail.com>
2024-05-07 11:39:56 -0600

    Finished the Webpack.config.js file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,45 +3,65 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-const wbplugin = require ('workbox-webpack-plugin')
-// TODO: Add CSS loaders and babel to webpack.
-const cssPlugin = require("mini-css-extract-plugin");
-const { webpack } = require('webpack');
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new webpackPlugin ({
-        template: "./index.html",
-        title: "Webpack Plugin"
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
       }),
-      new cssPlugin(),
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
-        fingureprints: false,
+        fingerprints: false,
         inject: true,
-        name: "My Text Pad",
-        sho
-      
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
